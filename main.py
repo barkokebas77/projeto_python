2c448d7b1b7aa679a8afec1e70fc13f4eedff992
Guilherme Barkokebas <guibarkokebas@gmail.com>
2025-06-08 02:18:56 -0300

    adicionando

--- main.py
+++ main.py
@@ -1,39 +1,40 @@
 import random
 
 def jogar_forca():
-    palavras = []
+    palavras = ['computador', 'monitor', 'controle']
     palavra_secreta = random.choice(palavras)
     letras_corretas = ['_'] * len(palavra_secreta)
-    tentativas = 6
+    tentativas = 5
     letras_erradas = []
 
-    print("Bem-vindo ao Jogo da Forca!")
-    print("A palavra secreta tem", len(palavra_secreta), "letras.")
+    print("Bem vindo ao Jogo da Forca")
+    print("A palavra secreta tem", len(palavra_secreta), "letras")
 
     while tentativas > 0:
-        print("Palavra: ", " ".join(letras_corretas))
+        print("Palavra:", "".join(letras_corretas))
         print("Tentativas restantes:", tentativas)
-        print("Letras erradas:", ", ".join(letras_erradas))
+        print("Letras erradas:", ",".join(letras_erradas))
 
-        letra = input("Digite uma letra: ").lower()
+        letra = input("Digite uma letra:").lower()
 
         if letra in letras_erradas or letra in letras_corretas:
-            print("Você já tentou essa letra.")
+         print("Você já tentou essa letra.")
         elif letra in palavra_secreta:
-            print("Boa! A letra está na palavra.")
-            for i in range(len(palavra_secreta)):
-                if palavra_secreta[i] == letra:
-                    letras_corretas[i] = letra
+           print("Boa! A letra está na palavra.")
+           for i in range(len(palavra_secreta)):
+              if palavra_secreta[i] == letra:
+                 letras_corretas[i] = letra
+
         else:
-            letras_erradas.append(letra)
-            tentativas -= 1
+           letras_erradas.append(letra)
+           tentativas -= 1
 
         if '_' not in letras_corretas:
-            print("Parabéns! Você adivinhou a palavra:", palavra_secreta)
-            break
+           print("Parabéns! Você advinhou a palavra:", palavra_secreta)
+           break
 
-    if tentativas == 0:
-        print("Você perdeu! A palavra era:", palavra_secreta)
+        if tentativas == 0:
+           print("Você perdeu! A palavra era:", palavra_secreta)
 
 if __name__ == "__main__":
-    jogar_forca()
+   jogar_forca()             
